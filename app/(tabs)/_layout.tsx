39b6acf7fe61e7a24861c0a8a3c85db773937182
faizaldr <fdr00zenc@gmail.com>
2025-11-06 11:16:02 +0700

    ok

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,38 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Tabs } from 'expo-router';
+import React from 'react';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
+      initialRouteName="add"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name="add"
         options={{
-            title: 'Tambah',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="location.fill" color={color} />,
+          title: 'Tambah',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="location.fill" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Daftar',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="list.number" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="list.number" color={color} />
+          ),
         }}
       />
     </Tabs>
