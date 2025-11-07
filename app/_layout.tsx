2df4460c6f3be46d99f44d2419afd886f2c62b0d
faizaldr <fdr00zenc@gmail.com>
2025-11-07 09:54:38 +0700

    ok

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,7 +15,8 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack initialRouteName='login'>
+        <Stack.Screen name='login' options={{ title: "Login" }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
